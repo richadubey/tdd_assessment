acd6311ff1fab97840e367768732fd3584c5873d
Richa Dubey <richadubey39@gmail.com>
2024-02-27 21:34:31 +0530

    Added the changes for condition

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,14 +7,12 @@ class StringCalculator
       delimiter = ','
     end
 
-    # Modify the regular expression to split by either the custom delimiter or a newline character
     numbers = numbers.to_s.gsub('"', '').split(/#{delimiter}|,|\n/).map(&:to_i)
     negatives = numbers.select { |num| num < 0 }
 
     if negatives.any?
       puts "Negatives not allowed: #{negatives.join(', ')}"
     else
-      puts "Checking numbers: #{numbers}"
       puts numbers.sum
     end
   end
@@ -23,3 +21,4 @@ end
 puts "Please enter string:"
 user_input = gets.chomp
 StringCalculator.add(user_input)
+StringCalculator.add("1\n2,3")
