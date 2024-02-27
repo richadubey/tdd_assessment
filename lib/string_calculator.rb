606659b40b2b61e92906a9c63265ba9a35cd8972
Richa Dubey <richadubey39@gmail.com>
2024-02-27 19:22:54 +0530

    Added the changes for adding number in one string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,1 +1,14 @@
-program file
+
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty? || numbers.nil?
+    puts numbers
+    numbers = numbers.split(",").map(&:to_i)
+    puts "Checking number #{numbers}"
+    puts numbers.sum
+  end
+end
+puts "Please enter string:"
+
+user_input = gets.chomp
+StringCalculator.add(user_input)
