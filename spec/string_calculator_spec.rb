68f622fd472655c0e79dca2ef830a808b6bc7f70
Richa Dubey <richadubey39@gmail.com>
2024-02-27 21:40:26 +0530

    Added rspec for negative & empty condition

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,1 +1,17 @@
-This is spec file
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context 'when given negative numbers' do
+      it 'raises an exception' do
+        expect { StringCalculator.add("1,-2,3") }.to raise_error(RuntimeError, "Negatives not allowed: -2")
+      end
+    end
+  end
+end
