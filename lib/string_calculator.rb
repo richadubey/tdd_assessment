d3d1d5da18cee1a60e732e0d2718cd0dd3e5fadd
Richa Dubey <richadubey39@gmail.com>
2024-02-27 20:10:45 +0530

    Added changes for negative & delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,25 @@
-
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty? || numbers.nil?
-    puts numbers
-    numbers = numbers.split(",").map(&:to_i)
-    puts "Checking number #{numbers}"
-    puts numbers.sum
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+    else
+      delimiter = ','
+    end
+
+    # Modify the regular expression to split by either the custom delimiter or a newline character
+    numbers = numbers.to_s.gsub('"', '').split(/#{delimiter}|,|\n/).map(&:to_i)
+    negatives = numbers.select { |num| num < 0 }
+
+    if negatives.any?
+      puts "Negatives not allowed: #{negatives.join(', ')}"
+    else
+      puts "Checking numbers: #{numbers}"
+      puts numbers.sum
+    end
   end
 end
-puts "Please enter string:"
 
+puts "Please enter string:"
 user_input = gets.chomp
 StringCalculator.add(user_input)
