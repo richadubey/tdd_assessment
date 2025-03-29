19d82a7250824f931ee09ba2b6bcab5648cb78c0
Richa Dubey <richadubey39@gmail.com>
2025-03-29 14:17:36 +0530

    Handle single number input in StringCalculator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,8 +2,16 @@ require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.add("")).to eq(0)
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context 'when given a single number' do
+      it 'returns the number itself' do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
     end
   end
 end
